26132c1fa9de63ae0b7d094c056aff166d500cf4
haSaJa <samhsj@naver.com>
2021-08-24 22:56:25 +0900

    Making WS Server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,12 +11,16 @@ app.use('/public',express.static(__dirname + '/public'));
 
 app.get("/", (req,res) => {
     res.render("home");
-})
+});
 app.get("/*",(req,res)=>{
     // 다른 모든 경로를 홈으로 돌려보냄
     res.redirect("/");
-})
+});
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+
+const websocketServer = new WebSocket.Server({ server });
+
+server.listen(3000, () => {
     console.log(`Listening on http://localhost:3000/`);
 });
